92a5e940013f5d9ab9174c1d8d8f243933e9e7fe
Deivis <deivis@dtm.debiab.com>
2024-01-11 05:34:26 -0500

    DTM try 1

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'obsidian',
+    LANGUAGE: 'es',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.htm',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt'
+};
